85588ac1912ea3054a85464e62a32103edf75c07
PrensCin <118314211+cinprens@users.noreply.github.com>
2025-06-21 19:01:01 +0300

    Mobil duzenlemeler ve test

--- __tests__/layout.test.js
+++ __tests__/layout.test.js
@@ -6,3 +6,15 @@ test('esnek genislik degiskenleri',()=>{
   expect(css).toMatch(/--left-panel-width/);
   expect(css).toMatch(/--right-panel-width/);
 });
+
+test('mobil sorgu mevcut',()=>{
+  expect(css).toMatch(/@media \(max-width: 600px\)/);
+});
+
+test('mobilde resizer gizli',()=>{
+  expect(css).toMatch(/\.resizer\s*{[^}]*display:\s*none/);
+});
+
+test('mobilde banner statik',()=>{
+  expect(css).toMatch(/position:\s*static/);
+});
