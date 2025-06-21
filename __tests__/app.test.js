2d6a276e6bbbc2759d22129e6430a36160c12574
PrensCin <118314211+cinprens@users.noreply.github.com>
2025-06-21 17:21:27 +0300

    Arayüz düzenlendi ve zamanlayıcı güncellendi

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,6 +4,6 @@ beforeAll(()=>{html=fs.readFileSync('index.html','utf8');});
 test('ana paneller mevcut',()=>{
   document.body.innerHTML=html;
   expect(document.getElementById('left')).not.toBeNull();
-  expect(document.getElementById('center')).not.toBeNull();
+  expect(document.getElementById('mid')).not.toBeNull();
   expect(document.getElementById('right')).not.toBeNull();
 });
