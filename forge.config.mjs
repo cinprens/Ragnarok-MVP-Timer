28d0a69720531f934bcaf0c55ff3e9d9859a72ed
PrensCin <118314211+cinprens@users.noreply.github.com>
2025-06-23 23:14:56 +0300

    Update forge.config.mjs

--- forge.config.mjs
+++ forge.config.mjs
@@ -4,11 +4,22 @@ import { MakerDeb } from '@electron-forge/maker-deb';
 import { MakerRpm } from '@electron-forge/maker-rpm';
 
 export default {
-  packagerConfig: {},
+  packagerConfig: {
+    executableName: 'RagnarokMvpTimer',
+    win32metadata: {
+      CompanyName: 'Senin İsmin veya Takma Adın',
+      FileDescription: 'Ragnarok MVP Timer – Zaman Takip Uygulaması',
+      ProductName: 'Ragnarok MVP Timer'
+    }
+  },
   rebuildConfig: {},
   makers: [
-    new MakerSquirrel({}),
-    new MakerZIP({}, ['darwin']),
+    new MakerSquirrel({
+      name: 'ragnarok_mvp_timer',
+      setupIcon: './build/icon.ico', // İkon eklemek istersen buraya yerleştir
+      loadingGif: './build/loading.gif' // opsiyonel, hoş bir geçiş görseli istersen
+    }),
+    new MakerZIP({}, ['win32']),
     new MakerDeb({}),
     new MakerRpm({})
   ]
